93fce250799e01e9296236ec7b3a22209635dc2e
Erperejildo <info@danielrodriguez.eu>
2015-08-12 10:23:56 +0200

    Structure created

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name repoWebdevForInfoDanielrodriguezeuApp
+ * @description
+ * # repoWebdevForInfoDanielrodriguezeuApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('repoWebdevForInfoDanielrodriguezeuApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name repoWebdevForInfoDanielrodriguezeuApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the repoWebdevForInfoDanielrodriguezeuApp
+ */
+angular.module('repoWebdevForInfoDanielrodriguezeuApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
